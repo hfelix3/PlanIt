a9808c905f98d7891f28ae5a03a18749029ac180
Hector <141383937+hfelix3@users.noreply.github.com>
2023-11-27 22:10:57 -0700

    started code for server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const sequalize = require('../config/connection')
+const sequalize = require('../config/connection');
 
 const app =express();
 const PORT = process.env.PORT || 3001;
