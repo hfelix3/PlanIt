3052786a12f422afaeabe95c9864fd9373753272
Hector <141383937+hfelix3@users.noreply.github.com>
2023-11-30 20:21:03 -0700

    revised the model for appintments

--- models/appointments.js
+++ models/appointments.js
@@ -1,24 +1,42 @@
 //TODO: model for data being stored in table
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../models/appointments');
 
-const appointment = sequelize.define('user', {
+class appointment extends Model {}
+
+appointment.init(
+    {
+        // primary key
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
     // attribute columns
-    name: {
-        type: sequelize.STRING
+        name: {
+            type: sequelize.STRING
+        },
+        phoneNumber: {
+            type: sequelize.INTEGER
+        },
+        barber: {
+            type: sequelize.STRING
+        },
+        date: {
+            type: sequelize.DATE
+        },
+        time: {
+            type: sequelize.TIME
+        },
     },
-    phoneNumber: {
-        type: sequelize.INTEGER
-    },
-    barber: {
-        type: sequelize.STRING
-    },
-    date: {
-        type: sequelize.DATE
-    },
-    time: {
-        type: sequelize.TIME
-    },
-    
-});
+    {
+        sequelize, 
+        timestamps: false,
+        // Prevent sequelize from renaming the table
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'appointment'
+    }
+);
 
 module.exports = appointment;
