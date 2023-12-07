ff7c5ed76a1dd47124e94ec8821c804fc4a4dd91
Pablo Rivera <pablomrivera@outlook.com>
2023-12-06 20:42:30 -0700

    fix authentication issues

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ const sequelize = require ('./config/connection');
 const exphbs = require('express-handlebars');
 const seedDatabase = require('./seeds/seed');
 const session = require('express-session');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
+const auth = require('./utils/auth');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ auth });
 
 const sess = {
     secret: 'Super secret secret',
