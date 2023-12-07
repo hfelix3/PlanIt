b5a2987bf7da91f7cc72399bf4e06ce0f0803a8b
Hector <141383937+hfelix3@users.noreply.github.com>
2023-12-06 20:11:15 -0700

    adding apikey protection

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const exphbs = require('express-handlebars');
 const seedDatabase = require('./seeds/seed');
 const session = require('express-session');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
